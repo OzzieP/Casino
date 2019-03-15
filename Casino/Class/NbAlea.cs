3328fedf5bdc6d140451018fd55c0101ae570090
brenz42 <48325418+brenz42@users.noreply.github.com>
2019-03-15 12:03:39 +0100

    ceci est un test

---
+++ Casino/Class/NbAlea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Class
+{
+    public class NbAlea
+    {
+        public int nombre;
+
+        public NbAlea()
+        {
+            Random rand = new Random();
+            this.nombre = rand.Next(1, 5);
+        }
+    }
+}
---
+++ Casino/Controller/ControllerNbAlea.cs
@@ -0,0 +1,25 @@
+﻿using Casino.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Casino.Controller
+{
+    public class ControllerNbAlea
+    {
+        string[] Partie()
+        {
+            string[] tirage = new string[3];
+            int i;
+            for (i = 0; i< 3; i++)
+            {
+                tirage[i] = NbAlea.NbAlea();
+            }
+
+            return tirage;
+        } 
+    }
+}
