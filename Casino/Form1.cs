33da536209856e43de19ff1f22e36db263a8002f
Dywaris <48318182+Dywaris@users.noreply.github.com>
2019-03-15 12:33:05 +0100

    Add files via upload

--- Casino/Form1.cs
+++ Casino/Form1.cs
@@ -16,5 +16,37 @@ namespace Casino
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // affichage valeur 1 dans label1
+
+        // affichage valeur 2 dans le label2
+
+        // affichage valeur 3 dans le label3
+
+
+        // lance la machine a $$$ && utlisation du controller && retirer argent au joueur
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // nombre de tirage souhaité (mode 2 )
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // ajouter de l'argent au joueur
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // afficher dans le label9 gagné ou perdu !
+        // affichage du solde joueur dans le label8
     }
 }
